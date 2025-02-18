e7050e0fec423b3ebcf59f081f92ecbdbc815427
Neil <146771532+neilvn@users.noreply.github.com>
2025-02-18 01:10:38 -0800

    added search

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'; 
-import { Text, AppShell, Burger, Group, NavLink, Title } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import Items from './testData.json';
-import { IconBox } from '@tabler/icons-react'; 
-import '@mantine/core/styles.css';
-import { Chatbox } from './ChatBox';
+import { useState } from 'react' 
+import { AppShell, Burger, Group, NavLink, Title, TextInput, Highlight } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
+import Items from './testData.json'
+import { IconBox } from '@tabler/icons-react' 
+import '@mantine/core/styles.css'
+import { Chatbox } from './ChatBox'
 
 export function App() {
-  const [opened, { toggle }] = useDisclosure();
+  const [opened, { toggle }] = useDisclosure()
+  const [searchValue, setSearchValue] = useState<string>('')
   const [selected, setSelected] = useState(Items[0].section)
 
   const sections = Items.map(item => item.section)
 
   const selectedItem = Items.find(item => item.section === selected)
 
+  const selectedParagraphs = searchValue
+    ? selectedItem?.content.filter(p => p.includes(searchValue))
+    : selectedItem?.content
+
   return (
     <AppShell
       header={{ height: 60 }}
       navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
-      padding="md"
+      padding='md'
     >
       <AppShell.Header>
-        <Group h="100%" px="md">
-          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-          <IconBox size={40} color='#6495ED'/>
-          <Title c='#6495ED'>Chatbot</Title>
+        <Group justify='space-between' align='initial' h={60}>
+          <Group h='100%' px='md'>
+            <Burger opened={opened} onClick={toggle} hiddenFrom='sm' size='sm' />
+            <IconBox size={40} color='#6495ED'/>
+            <Title c='#6495ED'>Chatbot</Title>
+          </Group>
+          <TextInput
+            value={searchValue}
+            onChange={e => setSearchValue(e.currentTarget.value)}
+            placeholder='Search'
+            mr={20}
+          />
         </Group>
       </AppShell.Header>
-      <AppShell.Navbar p="md">
+
+      <AppShell.Navbar p='md'>
         {sections.map(section =>
           <NavLink
             label={section}
@@ -37,10 +51,15 @@ export function App() {
           />
         )}
       </AppShell.Navbar>
+
       <AppShell.Main>
-        <Text size='xl'>{selectedItem?.content}</Text>
+        {selectedParagraphs
+         && selectedParagraphs.map(
+          (paragraph, idx) =>
+            <Highlight highlight={searchValue} size='xl' mb={20} key={idx}>{paragraph}</Highlight>
+        )}
         <Chatbox />
       </AppShell.Main>
     </AppShell>
-  );
+  )
 }
